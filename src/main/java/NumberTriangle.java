e89342c7c70f5b686a614f55fefa83682e025302
JohnJDH <johndh.jiang@mail.utoronto.ca>
2025-10-03 22:21:36 -0400

    Retrieve function implemented

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -94,23 +94,22 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-//        NumberTriangle current = this;
-//
-//        for (int i = 0; i < path.length(); i++) {
-//            char direction = path.charAt(i);
-//            if (direction == 'l') {
-//                current = current.left;
-//            } else if (direction == 'r') {
-//                current = current.right;
-//            }
-//
-//            if (current == null) {
-//                throw new IllegalArgumentException("Invalid path: node is null at position " + i);
-//            }
-//        }
-//
-//        return current.getRoot();
-        return -1;
+        NumberTriangle current = this;
+
+        for (int i = 0; i < path.length(); i++) {
+            char direction = path.charAt(i);
+            if (direction == 'l') {
+                current = current.left;
+            } else if (direction == 'r') {
+                current = current.right;
+            }
+
+            if (current == null) {
+                throw new IllegalArgumentException("Invalid path: node is null at position " + i);
+            }
+        }
+
+        return current.getRoot();
     }
 
     /** Read in the NumberTriangle structure from a file.
